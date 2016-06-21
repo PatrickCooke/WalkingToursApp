30b5a0b8c31a214f3a580d217a5f75edc908eaa8
PatrickCooke <cookepa1@gmail.com>
2016-06-21 10:29:34 -0400

    added point callout, adding info to fields is work in progress

--- WalkingToursApp/WayPointViewController.swift
+++ WalkingToursApp/WayPointViewController.swift
@@ -191,39 +191,30 @@ class WayPointViewController: UIViewController, MKMapViewDelegate {
             
             // Address dictionary
             print(placeMark.addressDictionary)
+            let addDict = placeMark.addressDictionary
             
-            // Location name
-            guard let locationName = placeMark.addressDictionary!["Name"] as? NSString else {
+            //Address Info
+            guard let streetNum = addDict!["SubThoroughfare"] as? NSString else{
                 return
             }
-            print("locationName \(locationName)")
-            // Street address
-            guard let streetNum = placeMark.addressDictionary!["SubThoroughfare"] as? NSString else{
-                return
-            }
-            // Street
-            guard let streetName = placeMark.addressDictionary!["Thoroughfare"] as? NSString else{
+            guard let streetName = addDict!["Thoroughfare"] as? NSString else{
                 return
             }
             let street = "\(streetNum) \(streetName)"
             print("street: \(street)")
-            // City
-            guard let city = placeMark.addressDictionary!["City"] as? NSString else {
+            guard let city = addDict!["City"] as? NSString else {
                 return
             }
             print("city - \(city)")
-            // Zip code
-            guard let zip = placeMark.addressDictionary!["ZIP"] as? NSString else {
+            guard let zip = addDict!["ZIP"] as? NSString else {
                 return
             }
             print("zip: \(zip)")
-            // State
-            guard let state = placeMark.addressDictionary!["State"] as? NSString else {
+            guard let state = addDict!["State"] as? NSString else {
                 return
             }
             print(state)
-            // Country
-            guard let country = placeMark.addressDictionary!["Country"] as? NSString else {
+            guard let country = addDict!["Country"] as? NSString else {
                 return
             }
             print(country)
@@ -235,32 +226,106 @@ class WayPointViewController: UIViewController, MKMapViewDelegate {
             pin.title = "\(street), \(city), \(state) \(zip)"
             pin.subtitle = country as String
             
-            
-            
             self.wpMapView.addAnnotation(pin)
             self.wpMapView.showAnnotations(self.wpMapView.annotations, animated: true)
-            
         })
         
     }
     
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        var view = mapView.dequeueReusableAnnotationViewWithIdentifier("id")
-        if view == nil {
-            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
-            view!.canShowCallout = true
-        } else {
-            view!.annotation = annotation
+    //MARK: - Apple Local Search
+    
+    @IBAction func localSearch() {
+        self.wpMapView.removeAnnotations(self.wpMapView.annotations)
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = wpNameTxtField.text
+        request.region = wpMapView.region
+        
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { (response, error) in
+            guard let response = response else {
+                print("Search error: \(error)")
+                return
+            }
+            for item in response.mapItems {
+                guard let name = item.name else {
+                    return
+                }
+                let location = item.placemark
+                let addDict = item.placemark.addressDictionary
+                print(name)
+                //Address Info
+                
+                guard let streetNum = addDict!["SubThoroughfare"] as? NSString else{
+                    return
+                }
+                guard let streetName = addDict!["Thoroughfare"] as? NSString else{
+                    return
+                }
+                let street = "\(streetNum) \(streetName)"
+                print("street: \(street)")
+                guard let city = addDict!["City"] as? NSString else {
+                    return
+                }
+                print("city - \(city)")
+                guard let zip = addDict!["ZIP"] as? NSString else {
+                    return
+                }
+                print("zip: \(zip)")
+                guard let state = addDict!["State"] as? NSString else {
+                    return
+                }
+                print(state)
+                
+                let pin = MKPointAnnotation()
+                pin.coordinate = location.coordinate
+                pin.title = name
+                pin.subtitle = street
+                
+                
+                
+                self.wpMapView.addAnnotation(pin)
+                self.wpMapView.showAnnotations(self.wpMapView.annotations, animated: true)
+            }
         }
         
-        view?.leftCalloutAccessoryView = nil
-        view?.rightCalloutAccessoryView = UIButton(type: UIButtonType.ContactAdd )
-        return view
     }
     
-    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        if (control as? UIButton)?.buttonType == UIButtonType.ContactAdd {
-            print("pressed")
+    //MARK: - Map Annotation Methods
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "pin"
+        var pin = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseIdentifier) as? MKPinAnnotationView
+        if pin == nil {
+            pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            pin!.pinTintColor = UIColor.redColor()
+            pin!.canShowCallout = true
+            pin!.rightCalloutAccessoryView = UIButton(type: .ContactAdd)
+        } else {
+            pin!.annotation = annotation
+        }
+        return pin
+    }
+    
+    
+    func mapView(mapView: MKMapView, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == annotationView.rightCalloutAccessoryView {
+            guard let pin = annotationView.annotation else {
+                return
+            }
+            print(pin.coordinate.latitude)
+            print(pin.coordinate.longitude)
+            wpLatTxtField.text = String(pin.coordinate.latitude)
+            wpLonTxtField.text = String(pin.coordinate.longitude)
+            if let title = pin.title {
+            print("title: \(title)")
+            }
+            if let subtitle = pin.subtitle {
+            print("subtitle: \(subtitle)")
+                wpaddressTxtField.text = subtitle
+            }
+            
+//            let app = UIApplication.sharedApplication()
+//            app.openURL(NSURL(string: (annotationView.annotation!.subtitle!)!)!)
         }
     }
     
@@ -268,7 +333,7 @@ class WayPointViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        wpMapView.delegate = self
         
         if let selWP = selectedWP {
             if let stopnum = selWP.wpStopNum{
